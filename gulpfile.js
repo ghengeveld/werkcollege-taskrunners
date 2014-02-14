c291fb690c18adb68c5ef11f2cce67b84d4b3dfd
Gert Hengeveld <gert@finalist.com>
2014-02-14 10:25:26 +0100

    Create `gulp clean` task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 // Load dependencies
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var clean = require('gulp-clean');
 
 gulp.task('sass', function() {
   return gulp.src('src/styles/app.scss')
     .pipe(sass())
     .pipe(gulp.dest('src/styles'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src('src/styles/app.css', { read: false })
+    .pipe(clean());
 });
