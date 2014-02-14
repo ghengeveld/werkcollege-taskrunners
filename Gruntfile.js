6a55fc06dc46644311853d98c52385d243cfebbb
Gert Hengeveld <gert@finalist.com>
2014-02-14 11:59:56 +0100

    Create `grunt build` task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,15 +7,53 @@ module.exports = function(grunt) {
         files: {
           'src/styles/app.css': 'src/styles/app.scss'
         }
+      },
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/styles',
+          src: ['*.scss', 'bootstrap.css'],
+          dest: 'dist/styles',
+          ext: '.css'
+        }]
       }
     },
     clean: {
-      dev: ['src/styles/app.css']
+      dev: ['src/styles/app.css'],
+      dist: ['dist/*']
+    },
+    uglify: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/scripts',
+          src: ['*.js'],
+          dest: 'dist/scripts'
+        }]
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'dist/index.html': 'src/index.html'
+        }
+      }
     }
   });
 
   // Load dependencies
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  grunt.registerTask('build', ['clean:dist', 'sass:dist', 'uglify:dist', 'htmlmin:dist']);
 
 };
